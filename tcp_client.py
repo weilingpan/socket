4f79f8a84bc3f8de0a4e62ef1d313cb028537a33
WeiLing Pan <zz860530@gmail.com>
2023-07-27 16:32:32 +0800

    docs: add comments

--- tcp_client.py
+++ tcp_client.py
@@ -4,6 +4,7 @@ HOST = '192.168.1.108'
 PORT = 5000
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #創建
+#socket.AF_INET 表示使用 Internet Protocol 的通訊協定，而 socket.SOCK_STREAM 表示傳輸方式為 TCP
 s.connect((HOST, PORT))
 
 # 使用者輸入的訊息給伺服器
--- udp_server.py
+++ udp_server.py
@@ -4,6 +4,7 @@ HOST = '192.168.1.108'
 PORT = 7000
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#socket.AF_INET 表示使用 Internet Protocol 的通訊協定，而 socket.SOCK_DGRAM 表示傳輸方式為 UDP
 s.bind((HOST, PORT))
 
 print('server start at: %s:%s' % (HOST, PORT))
